4ae36421af8acae26eb535ea85fd80a7b4d20321
showayamada <yamasho082433@gmail.com>
2023-04-11 16:21:59 +0900

    プレイヤーを設定

--- hello.py
+++ hello.py
@@ -1,18 +1,47 @@
 import pygame
+from pygame import mixer
 import sys
 
 pygame.init()                                   # Pygameの初期化
-screen = pygame.display.set_mode((400, 300))    # 大きさ400*300の画面を生成
-pygame.display.set_caption("Test")              # タイトルバーに表示する文字
+screen = pygame.display.set_mode((800, 600))    # 大きさ800*600の画面を生成
+# screen.fill((150, 150, 150))                    #背景の塗りつぶし
+pygame.display.set_caption("Invadars Game")              # タイトルバーに表示する文字
 
-while (1):
-    screen.fill((0,0,0))        # 画面を黒色(#000000)に塗りつぶし
-    pygame.display.update()     # 画面を更新
+# Player
+PlayerImg = pygame.image.load("player.png")
+playerX, playerY = 370, 480
+player_change = 0
+
+def player(x, y):
+    screen.blit(PlayerImg, (x, y))
+
+#音声の出力:ぴゅん
+# mixer.Sound("laser.wav").play()
+
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    # font = pygame.font.SysFont(None, 80)
+    # message = font.render("Hello World", False, (255, 255, 255))
+    # screen.blit(message, (20, 50))
     # イベント処理
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:  # 閉じるボタンが押されたら終了
-            pygame.quit()       # Pygameの終了(画面閉じられる)
-            sys.exit()
+            running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -1.5
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 1.5
+            
+
+    # playerX += 1.5
+    player(playerX, playerY)
+
+    pygame.display.update()
 
 
 
